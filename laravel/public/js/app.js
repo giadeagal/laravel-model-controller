de958b0dcd72c70f86c6524c056ddf7197995eb3
Gianluca <gl.galassini@gmail.com>
2021-09-07 15:41:42 +0200

    aggiustamenti grafici

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/gianlucagalassini/Documents/boolean/esercitazioni/mamp_public/laravel-comics/laravel-comics/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/gianlucagalassini/Documents/boolean/esercitazioni/mamp_public/laravel-comics/laravel-comics/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/gianlucagalassini/Documents/boolean/esercitazioni/mamp_public/laravel-model-controller/laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/gianlucagalassini/Documents/boolean/esercitazioni/mamp_public/laravel-model-controller/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
